e677ca379b3722b964538c2dc956fa535095f6b9
Hamin Park <haeeema@naver.com>
2023-11-16 17:07:07 +0900

    Time: 57 ms (65.61%), Space: 44.5 MB (65.97%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+
+
+var createCounter = function(init) {
+    let temp = init;
+    const increment = function() {
+        temp = temp + 1;
+        return temp
+    }
+    const decrement = function() {
+        temp = temp - 1;
+        return temp;
+    }
+    const reset = function() {
+        temp = init;
+        return temp;
+    }
+    return { increment, decrement, reset };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
